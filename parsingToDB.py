f436e4ec9ef3c3758574f8a1fc6288bfc8f41c3e
EMA\bkt5031 <bkt5031@psu.edu>
2018-12-05 15:18:38 -0500

    parseIntoDB

---
+++ parsingToDB.py
@@ -0,0 +1,44 @@
+		#!/usr/bin/python
+
+import pandas as pd
+from matplotlib import pyplot as plt
+import os.path
+import pyodbc
+import pysftp
+import urllib.parse 
+
+sql_conn = pyodbc.connect('DRIVER={SQL Server}; SERVER=UAO-NEBULA; DATABASE=Staging; Authentication=SQL SERVER Authentication; Login=cfserver; Password=cf80767468; Trusted_Connection=yes') 
+# query = "SELECT * FROM [STAGING].[dbo].[TalismaLeadCards]"
+
+query = """INSERT INTO [STAGING].[dbo].TalismaLeadCards
+           ([first_name], [middle_initial], [last_name])
+           VALUES ('b', 'k', 't')"""
+
+# If your driver's cursors don't work as context managers, wrap with
+# contextlib.closing
+# with sql_conn.cursor() as cursor:
+    # cursor.executemany(query, results)
+df = pd.read_sql(query, sql_conn)
+# print(df)
+df.head(5)
+		# sql_conn.commit()
+
+# query = "insert into [STAGING].[dbo].TalismaLeadCards([first_name], [middle_initial], [last_name]) Values ('brian', 'k', 'tran');"
+
+
+save_here = 'C:/Users/bkt5031/Desktop/dirPath'
+
+filename = 'C:/CommonApp/CommonApp_Prospect.txt'
+# open(filename, mode='r')
+r = 'r'
+
+with open(filename, r) as file:  #  'r' is to read
+	w = 'w'
+	data = file.read()
+	completeName = os.path.join(save_here, 'prospectCommApp.txt')
+	file = open(completeName, w)
+	file.write(data)
+	# print (data)
+	file.close()
+
+# plt.plot(data.year, data.population)
