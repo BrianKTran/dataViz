e5fd1def02d114cd935c22c1f6d7351f00409b31
EMA\bkt5031 <bkt5031@psu.edu>
2018-12-11 13:36:09 -0500

    mysql, error temp fix, data viz and analyz, look into log, reg

--- dataVizImport.py
+++ dataVizImport.py
@@ -1,7 +1,9 @@
 # import pyodbc
 import pymysql
 import csv
-
+import pandas as pd
+from matplotlib import pyplot as plt
+import os.path
 # DESTINATION CONNECTION
 # drivr = "ODBC Driver 13 for SQL Server"
 # servr = "uao-nebula"
@@ -13,6 +15,38 @@ import csv
 
 my_cnxn = pymysql.connect(host='localhost', port=3306, user='bkt5031', passwd='Phili467', db='mydb')
 my_cursor = my_cnxn.cursor()
+
+
+save_here = 'C:/Users/bkt5031/Desktop/dirPath'
+filename = 'countries.csv'
+
+r = 'r'
+with open(filename, r) as file:     
+    w = 'w'
+    data = file.read()
+    completeName = os.path.join(save_here, filename)
+    file = open(completeName, w)
+    file.write(data)
+    # print (data)
+    file.close()
+# Compare the population growth in the US and China
+data = pd.read_csv(filename) 
+data.country == 'United States'
+
+us = data[data.country == 'United States']
+china = data[data.country == 'China']
+print(us)
+print(china)
+
+print(us.population / us.population.iloc[0] * 100)
+
+plt.plot(us.year, us.population / us.population.iloc[0] * 100, 'o')
+plt.plot(china.year, china.population / china.population.iloc[0] * 100, 'o')
+plt.legend(['United States', 'China'])
+plt.xlabel('year')
+plt.ylabel('population')
+plt.show()
+
 
 def insert_records(table, yourcsv, cursor, cnxn):
     #INSERT SOURCE RECORDS TO DESTINATION
@@ -30,3 +64,4 @@ table = 'mydb.import'
 mycsv = 'countries.csv' # SET YOUR FILEPATH
 insert_records(table, mycsv, my_cursor, my_cnxn)
 my_cursor.close()
+
